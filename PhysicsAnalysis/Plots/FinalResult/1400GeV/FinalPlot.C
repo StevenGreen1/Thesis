e43d7dc705b22b988e6d7c20c2912a18b4fc8df0
Steven Green <stevengreen@dhcp-10-248-125-202.eduroam.wireless.private.cam.ac.uk>
2017-01-26 11:48:48 +0000

    Updated final results plot for 1.4 TeV.

--- PhysicsAnalysis/Plots/FinalResult/1400GeV/FinalPlot.C
+++ PhysicsAnalysis/Plots/FinalResult/1400GeV/FinalPlot.C
@@ -1,14 +1,14 @@
-TGraph2D *SinglePlot(TString name);
+Traph2D *SinglePlot(TString name);
 
 //================================================================
 
 void FinalPlot()
 {
+    gStyle->SetLegendBorderSize(1); 
+
     TString name1 = "SPFOs_kt_0p90_Final_1400GeV_FitData.root";
 
-    TGraph2D *pTGraph2D_a = SinglePlot(name1,"Chi2CosThetaStarSynJets_vs_Bosons");
-    TGraph2D *pTGraph2D_b = SinglePlot(name1,"Chi2CosThetaStarSynJets");
-    TGraph2D *pTGraph2D_c = SinglePlot(name1,"Chi2CosThetaStarSynBosons");
+    TGraph2D *pTGraph2D = SinglePlot(name1,"Chi2CosThetaStarSynJets");
 
     TH1F *pTH1FDummy1 = new TH1F();
     pTH1FDummy1->SetLineColor(2);
@@ -16,6 +16,26 @@ void FinalPlot()
     pTH1FDummy2->SetLineColor(418);
     TH1F *pTH1FDummy3 = new TH1F();
     pTH1FDummy3->SetLineColor(4);
+/*
+    TH1F *pTH1FDummy4 = new TH1F();
+    pTH1FDummy4->SetLineColor(4);
+    TH1F *pTH1FDummy5 = new TH1F();
+    pTH1FDummy5->SetLineColor(880);
+*/
+    TLegend *pTLegend_all = new TLegend(0.2, 0.15, 0.6, 0.35);
+    pTLegend_all->AddEntry(pTH1FDummy1,"68\% Confidence Contour", "l");
+    pTLegend_all->AddEntry(pTH1FDummy2,"90\% Confidence Contour", "l");
+    pTLegend_all->AddEntry(pTH1FDummy3,"99\% Confidence Contour", "l");
+    pTLegend_all->SetLineWidth(2);
+    pTLegend_all->SetLineColor(kBlack);
+    pTLegend_all->SetFillColor(10);
+
+//    TExec *ex1 = new TExec("ex1","Int_t colors1[] = {0, 1, 2}; gStyle->SetPalette((sizeof(colors1)/sizeof(Int_t)), colors1);"); // Black
+//    TExec *ex2 = new TExec("ex2","Int_t colors2[] = {0, 2, 1}; gStyle->SetPalette((sizeof(colors2)/sizeof(Int_t)), colors2);"); // Red
+//    TExec *ex3 = new TExec("ex3","Int_t colors3[] = {0, 418, 1}; gStyle->SetPalette((sizeof(colors3)/sizeof(Int_t)), colors3);"); // Green
+//    TExec *ex4 = new TExec("ex4","Int_t colors4[] = {0, 4, 1}; gStyle->SetPalette((sizeof(colors4)/sizeof(Int_t)), colors4);"); // Blue
+//    TExec *ex5 = new TExec("ex5","Int_t colors5[] = {0, 880, 1}; gStyle->SetPalette((sizeof(colors5)/sizeof(Int_t)), colors5);"); // Violet
+
 /*
     TLine *pLineAlpha4High = new TLine(0.0093,-0.02,0.0093,0.02);
     pLineAlpha4High->SetLineColor(kBlack);
@@ -37,8 +57,6 @@ void FinalPlot()
     pLineAlpha5Low->SetLineStyle(2);
     pLineAlpha5Low->SetLineWidth(2);
 */
-    gStyle->SetLegendBorderSize(1); 
-
     TLegend *pTLegend_all = new TLegend(0.2, 0.15, 0.6, 0.35);
     pTLegend_all->AddEntry(pTH1FDummy1,"68\% Confidence Contour", "l");
     pTLegend_all->AddEntry(pTH1FDummy2,"90\% Confidence Contour", "l");
@@ -52,15 +70,16 @@ void FinalPlot()
     pTCanvas->SetLeftMargin(0.15);
     pTCanvas->SetTopMargin(0.10);
     pTCanvas->SetBottomMargin(0.10);
-    pTGraph2D_b->SetTitle("Longitudinally Invariant Kt, Selected PFOs, R = 0.9");
-    pTGraph2D_b->SetLineWidth(3);
-    pTGraph2D_b->Draw("CONT1");
-//    pLineAlpha4High->Draw();
-//    pLineAlpha4Low->Draw();
-//    pLineAlpha5High->Draw();
-//    pLineAlpha5Low->Draw();
+
+    pTGraph2D->SetTitle("Longitudinally Invariant Kt, Selected PFOs, R = 0.9");
+    pTGraph2D->Draw("CONT1");
+/*
+    pLineAlpha4High->Draw();
+    pLineAlpha4Low->Draw();
+    pLineAlpha5High->Draw();
+    pLineAlpha5Low->Draw();
+*/
     pTLegend_all->Draw();
-    pTCanvas->Update();
     pTCanvas->SaveAs("Final.pdf");
     pTCanvas->SaveAs("Final.C");
 }
@@ -88,20 +107,23 @@ TGraph2D *SinglePlot(TString name, TString quantity)
         pTGraph2D->SetPoint(pTGraph2D->GetN(), alpha4, alpha5, chi2);
     }
 
-    Int_t colors[] = {0, 2, 418, 4, 1};
-    gStyle->SetPalette((sizeof(colors)/sizeof(Int_t)), colors);
     Double_t levels[] = {0.0, 2.28, 4.61, 9.21, 1.79e308};
+    Int_t colors1[] = {0, 2, 418, 4, 1};
+    gStyle->SetPalette((sizeof(colors1)/sizeof(Int_t)), colors1);
+  
+//    Double_t levels[] = {0.0, 2.28, 1.79e308}; 
+//    Int_t colors1[] = {0, 1, 2}; 
+//    gStyle->SetPalette((sizeof(colors1)/sizeof(Int_t)), colors1);
     pTGraph2D->GetHistogram()->SetContour((sizeof(levels)/sizeof(Double_t)), levels);
-
     pTGraph2D->GetXaxis()->SetTitleSize(0.05);
-    pTGraph2D->GetXaxis()->SetLabelSize(0.05);
+    pTGraph2D->GetXaxis()->SetLabelSize(0.035);
     pTGraph2D->GetXaxis()->SetTitle("#alpha_{4}");
     pTGraph2D->GetYaxis()->SetTitleOffset(1.6);
     pTGraph2D->GetYaxis()->SetTitleSize(0.05);
-    pTGraph2D->GetYaxis()->SetLabelSize(0.05);
+    pTGraph2D->GetYaxis()->SetLabelSize(0.035);
     pTGraph2D->GetYaxis()->SetTitle("#alpha_{5}");
-    pTGraph2D->SetTitle("");
-
+    pTGraph2D->SetName(name + "_" + quantity);
+    delete pTFile;
     return pTGraph2D;
 }
 
