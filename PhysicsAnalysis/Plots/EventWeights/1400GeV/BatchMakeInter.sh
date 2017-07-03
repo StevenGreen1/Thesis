85fafcd18a72cd498a02ba067f5a3b3fae4534e4
Steven Green <stevengreen@dhcp-10-248-114-103.eduroam.wireless.private.cam.ac.uk>
2017-07-03 15:05:44 +0100

    Messing arounf with root colour scheme.

---
+++ PhysicsAnalysis/Plots/EventWeights/1400GeV/BatchMakeInter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in 02 09 14 16 44 51 78
+do
+    root -b -q "MakeAwesomePlotsInter.C(\"EventWeightsForEvent1000010${i}_1400GeV_SPFOs_kt_0p70_10Bins_Start_0_End_10_1400GeV_Interpolated\")"
+done
+
